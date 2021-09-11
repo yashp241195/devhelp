bca5c299630ad423f88808e00d4efe260e9ae894
yashp241195 <yashp241195@gmail.com>
2021-09-11 19:38:34 +0530

    first commit

--- devhelp.sh
+++ devhelp.sh
@@ -1,7 +1,18 @@
 declare -A options
 
 options["express"]="helpers/express.sh $2"
-options["react"]="helpers/react.sh $2"
 
 ${options[$1]}
+
+if [[ $1 == "--list" ]];
+then 
+for i in "${!options[@]}"; do echo " $i "; done
+elif [[ $1 == "--help" ]];
+then
+echo "
+Welcome to devhelp, a command line helper tool for faster development
+--list : list of all helpers
+"
+fi
+
 
